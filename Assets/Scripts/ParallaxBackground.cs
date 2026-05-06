91d24b8e6b53aae200c490d96c951d9a7af1eaf7
Vicky <98425423+BurningVicky@users.noreply.github.com>
2026-05-06 18:13:03 -0300

    parallax camera

--- Assets/Scripts/ParallaxBackground.cs
+++ Assets/Scripts/ParallaxBackground.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class ParallaxBackground : MonoBehaviour
 {
     private Camera mainCamera;
     private float lastCameraPositionX;
+    private float cameraHalfWidth;
 
 
     [SerializeField] private ParallaxLayer[] backgroundLayers;
@@ -11,17 +13,29 @@ public class ParallaxBackground : MonoBehaviour
     private void Awake()
     {
         mainCamera = Camera.main;
+        cameraHalfWidth = mainCamera.orthographicSize * mainCamera.aspect;
+        CalculateImageLength();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         float CurrentCameraPositionX = mainCamera.transform.position.x;
         float distanceToMove = CurrentCameraPositionX - lastCameraPositionX;
         lastCameraPositionX = CurrentCameraPositionX;
 
+        float cameraLeftEdge = CurrentCameraPositionX - cameraHalfWidth;
+        float cameraRightEdge = CurrentCameraPositionX + cameraHalfWidth;
+
         foreach(ParallaxLayer layer in backgroundLayers)
         {
             layer.Move(distanceToMove);
+            layer.LoopBackground(cameraLeftEdge, cameraRightEdge);
         }
+    }
+
+    private void CalculateImageLength()
+    {
+        foreach(ParallaxLayer layer in backgroundLayers)
+            layer.CalculateImageWidth();
     }
 }
